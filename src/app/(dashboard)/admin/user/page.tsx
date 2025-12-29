8ca8de96e7e4a2c11e42f812899bdb41adc76bea
Jefferson <jefferson.swanto@gmail.com>
2025-12-29 23:01:22 +0700

    feat: implement tanstack-query, and table UI for admin/user page

--- src/app/(dashboard)/admin/user/page.tsx
+++ src/app/(dashboard)/admin/user/page.tsx
@@ -1,3 +1,8 @@
+import UserManagement from "./_components/user";
+
+export const metadata = {
+  title: "WPU | User Management",
+};
 export default function UserPage() {
-  return <div>User Page</div>;
+  return <UserManagement />;
 }
