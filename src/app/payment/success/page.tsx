6a73a68bf0a39945c7721af64c454a2caa16c61e
Jefferson <jefferson.swanto@gmail.com>
2026-02-05 22:46:11 +0700

    feat: update status order and payment success/ failed ui

---
+++ src/app/payment/success/page.tsx
@@ -0,0 +1,9 @@
+import Success from "./_components/Success";
+
+export const metadata = {
+  title: "Cafe | Payment Success",
+};
+
+export default function SuccessPage() {
+  return <Success />;
+}
